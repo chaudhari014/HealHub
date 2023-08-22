cee7141bfe38059a2dd8895a79613bb9a8cb9aec
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-08-22 19:22:17 +0530

    navbar updated

--- Views/src/components/Navbar.jsx
+++ Views/src/components/Navbar.jsx
@@ -20,49 +20,90 @@ function Navbar() {
       </div>
       {/*2. Linked Blocks */}
       <div>
-        <ul
-          id="navbar"
-          className={barOrTabClicked ? "#navbar active" : "#navbar"}
-        >
-          <li>
-            <Link
-              onClick={handleBarOrTabClicked}
-              className="active hideHome"
-              to={"/"}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link onClick={handleBarOrTabClicked} to={"/find_doctors"}>
-              Find Doctors
-            </Link>
-          </li>
-          <li>
-            <Link onClick={handleBarOrTabClicked} to={"/video_consult"}>
-              Video Consult
-            </Link>
-          </li>
-          <li>
-            <Link onClick={handleBarOrTabClicked} to={"/services"}>
-              Our Services
-            </Link>
-          </li>
-          <li>
-            <Link onClick={handleBarOrTabClicked} to={"/"}>
-              Lab Tests
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={handleBarOrTabClicked}
-              className="loginButton"
-              to={"/signin"}
-            >
-              <button>Login</button>
-            </Link>
-          </li>
-        </ul>
+        {!barOrTabClicked ? (
+          <ul
+            id="navbar"
+            className={barOrTabClicked ? "#navbar active" : "#navbar"}
+          >
+            <li>
+              <Link className="active hideHome" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to={"/find_doctors"}>Find Doctors</Link>
+            </li>
+            <li>
+              <Link to={"/video_consult"}>Video Consult</Link>
+            </li>
+            <li>
+              <Link to={"/services"}>Our Services</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Lab Tests</Link>
+            </li>
+            <li>
+              <Link className="loginButton" to={"/signin"}>
+                <button>Login</button>
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul
+            id="navbar"
+            className={barOrTabClicked ? "#navbar active" : "#navbar"}
+          >
+            <li>
+              <div className="navbarTempData">
+                <p>
+                  HealHub <span>PLUS</span>
+                </p>
+                <p>Health plans for you and your family</p>
+              </div>
+            </li>
+            <li></li>
+
+            <li>
+              <Link onClick={handleBarOrTabClicked} to={"/find_doctors"}>
+                <i className="fa-solid fa-magnifying-glass"></i> Find Doctors -{" "}
+                <span>Book an appointment</span>
+              </Link>
+            </li>
+            <li>
+              <Link onClick={handleBarOrTabClicked} to={"/video_consult"}>
+                <i className="fa-solid fa-video"></i> Video Consult -{" "}
+                <span>Consult top Doctors</span>
+              </Link>
+            </li>
+            <li>
+              <Link onClick={handleBarOrTabClicked} to={"/"}>
+                <i className="fa-solid fa-cart-shopping"></i> Medicines -{" "}
+                <span>HealHub pharmacy</span>
+              </Link>
+            </li>
+            <li>
+              <Link onClick={handleBarOrTabClicked} to={"/services"}>
+                <i className="fa-solid fa-users-gear"></i> Our Services -{" "}
+                <span>Book our Services</span>
+              </Link>
+            </li>
+            <li>
+              <Link onClick={handleBarOrTabClicked} to={"/"}>
+                <i className="fa-solid fa-flask-vial"></i> Lab Tests -{" "}
+                <span>Book tests & checkup</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={handleBarOrTabClicked}
+                className="loginButton"
+                to={"/signin"}
+              >
+                <button>Login</button> 
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
 
       {/* Burger Menu */}
