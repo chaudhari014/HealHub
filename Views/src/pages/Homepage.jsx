9a9daa639169954844b4a5c8e773a12e01f9dac9
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-08-25 15:54:25 +0530

    home page updated again

--- Views/src/pages/Homepage.jsx
+++ Views/src/pages/Homepage.jsx
@@ -6,10 +6,12 @@ import "./Homepage.css";
 function Homepage() {
   return (
     <div className="HomePage">
+
       {/* Add 01 */}
       <div className="add01">
         <img src={banner01} alt="add01" />
       </div>
+      
       {/* Cards */}
       <div className="CardMain01">
         <Cards CardDetails={cardsData} />
