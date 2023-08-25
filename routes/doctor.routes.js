072d844dbe7bbd72d3c3f637bbe98cc5e2f66023
thesujalogy <thesujalogy@gmail.com>
2023-08-26 01:01:09 +0530

    adding .env

--- routes/doctor.routes.js
+++ routes/doctor.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllDoc } = require('../controllers/doctor.controller');
+const { getAllDoc, addDoctor } = require('../controllers/doctor.controller');
 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/doctors", getAllDoc);
+doctorRouter.post("/doctors", addDoctor);
 
 module.exports = {
     doctorRouter
