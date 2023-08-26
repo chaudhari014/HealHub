80ff37503ec5e37cfb815981753b473ebff5be01
chaudhari014 <fatarambhai.k14@gmail.com>
2023-08-26 22:11:55 +0530

    ok

--- Views/src/App.jsx
+++ Views/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import { Homepage, FindDoctors, Services, VideoConsult, SignIn } from "./pages";
 import { Navbar, Footer } from "./components";
 import SignUp from "./pages/SignUp";
-
 function App() {
   return (
     <div className="App">
@@ -15,7 +14,6 @@ function App() {
         <Route path="/video_consult" element={<VideoConsult />}></Route>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-
       </Routes>
       <Footer />
     </div>
