931b459956cd101938de8c345db4c6d146cadf51
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-08-28 12:36:25 +0530

    css updated of service and appointment file

--- Views/src/pages/FindDoctors.jsx
+++ Views/src/pages/FindDoctors.jsx
@@ -1,63 +1,69 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./FindDoctor.css";
 import API from "../Api";
-import { useLocation,useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function FindDoctors() {
   const location = useLocation();
   const expert = location.state.id;
-  console.log(expert)
+  console.log(expert);
 
   const navigate = useNavigate();
 
-  const dataHandler = (id) =>{
-     const drId = id;
-     onClickHandler(drId);
-  }
+  const dataHandler = (id) => {
+    const drId = id;
+    onClickHandler(drId);
+  };
 
-  const onClickHandler = (id) =>{
-    navigate('/appointment', {state:{drsId:id}})
-  }
-  
-  const [docData, setdocData] = useState([])
-  useEffect(()=>{
-    if(expert){
+  const onClickHandler = (id) => {
+    navigate("/appointment", { state: { drsId: id } });
+  };
+
+  const [docData, setdocData] = useState([]);
+  useEffect(() => {
+    if (expert) {
       fetch(`${API}/doctors`)
-      .then((res)=>res.json())
-      .then((myData)=>{
-        const ourData = myData.data
-        const fixData = ourData.filter((elem)=> elem.Speciality==expert)
-        setdocData(fixData)
-        console.log(ourData);
-      })
-      .catch((error)=>{
-        console.log(error)
-      })
+        .then((res) => res.json())
+        .then((myData) => {
+          const ourData = myData.data;
+          const fixData = ourData.filter((elem) => elem.Speciality == expert);
+          setdocData(fixData);
+          console.log(ourData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-    
-  },[])
-  
-  
-    
+  }, [expert]);
+
   return (
     <>
-    <h1>Our {expert} Doctors</h1>
-    <div  id="doc">
-     {
-      docData.map((el)=>(
-          <div id="card" key={el._id}>
-          <img src={el.image} alt="" />
-          <h3>{el.name}</h3>
-          <h4>{el.description}</h4>
-          <h5>Rating:- {el.rating}/5</h5>
-          <button onClick={()=>{dataHandler(el._id)}}>Book Appointment</button>
-         </div>
-        )
-        )
-     }
-     </div>
-     </>
-  )
+      <div className="findDoctors">
+        <h1>Our {expert} Doctors</h1>
+        <div id="doc">
+          {docData.map((el) => (
+            <div id="card" key={el._id}>
+              <div>
+                <img src={el.image} alt="err" />
+              </div>
+              <div>
+                <h3>{el.name}</h3>
+                <h4>{el.description}</h4>
+                <h5>Rating:- {el.rating}/5</h5>
+              </div>
+              <button
+                onClick={() => {
+                  dataHandler(el._id);
+                }}
+              >
+                Book Appointment
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FindDoctors;
