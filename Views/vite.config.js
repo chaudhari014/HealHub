56f7b534a25a52a32b684f272f88c82bd92d9f80
chaudhari014 <fatarambhai.k14@gmail.com>
2023-10-05 16:07:26 +0530

    build vercel

--- Views/vite.config.js
+++ Views/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["@stripe/react-stripe-js", "@stripe/stripe-js"],
+  },
+  build: {
+    outDir: "build", // Output directory set to "build"
   },
 });
